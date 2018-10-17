e72f38d89b0a23dc0903cb52b261bffa055b5db7
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-17 18:11:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,16 @@
 def my_collect(array)
   counter = 0 
   while counter > array.size 
-  yield(array.upcase)
+  yield(array.upcase) = up_array
   counter += 1 
 end 
+  return up_array
 end
-  
+  counter = 0 
+  while counter > array.size 
+  yield(array.split(" ").first) = first_array
+  counter += 1
+end
+  return first_array
+end
 
